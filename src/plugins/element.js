35593b8f3805db29795dd6990634cc86c6d75257
fancyistyle <fancyistyle@qq.com>
2020-02-28 14:51:27 +0800

    完成了权限功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import { Button,Message,Form,FormItem,Input,Container,
     Header,Aside,Main,Menu,Submenu,MenuItemGroup,MenuItem,
     Breadcrumb,BreadcrumbItem,Card,Row,Col,Table,TableColumn,
-    Switch,Tooltip,Pagination,Dialog,MessageBox
+    Switch,Tooltip,Pagination,Dialog,MessageBox,Tag,Tree,Option,
+    Select,
     
     } from 'element-ui'
 
@@ -12,6 +13,7 @@ Vue.use(Button).use(Form).use(FormItem).use(Input).use(Container)
     .use(Header).use(Aside).use(Main).use(Menu).use(MenuItemGroup)
     .use(MenuItem).use(Submenu).use(Breadcrumb).use(BreadcrumbItem)
     .use(Card).use(Row).use(Col).use(Table).use(TableColumn).use(Switch)
-    .use(Tooltip).use(Pagination).use(Dialog)
+    .use(Tooltip).use(Pagination).use(Dialog).use(Tag).use(Tree)
+    .use(Select).use(Option)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
